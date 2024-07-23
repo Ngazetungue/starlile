20bbf556709c8eb28ad10a53dfdd5626683a1f35
Ngazetungue <nmuheuez@gmail.com>
2024-07-23 21:51:02 +0200

    add functions to views

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from . views import home, about, fans
+from . views import home, about, fans, news_list, news_detail, news_create, news_update, news_delete
 
 urlpatterns = [
     path("", home, name="home"),
     path("about/", about, name="about"),
     path("fans/", fans, name="fans"),
+    
+
+    path('news/', news_list, name='news_list'),
+    path('news/<int:id>/', news_detail, name='news_detail'),
+    path('news/create/', news_create, name='news_create'),
+    path('news/update/<int:id>/', news_update, name='news_update'),
+    path('news/delete/<int:id>/', news_delete, name='news_delete'),
+
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.shortcuts import render
-from . models import Fixture
+from .forms import NewsForm
+from . models import Fixture, News
 
 
 def home(request):
@@ -11,3 +14,39 @@ def about(request):
 
 def fans(request):
     return render(request, "home/fans.html" )
+
+def news_list(request):
+    news = News.objects.all()
+    return render(request, 'home/news_list.html', {'news': news})
+
+def news_detail(request, id):
+    news_item = get_object_or_404(News, id=id)
+    return render(request, 'home/news_detail.html', {'news': news_item})
+
+def news_create(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('news_list')
+    else:
+        form = NewsForm()
+    return render(request, 'home/news_form.html', {'form': form})
+
+def news_update(request, id):
+    news_item = get_object_or_404(News, id=id)
+    if request.method == 'POST':
+        form = NewsForm(request.POST, request.FILES, instance=news_item)
+        if form.is_valid():
+            form.save()
+            return redirect('news_list')
+    else:
+        form = NewsForm(instance=news_item)
+    return render(request, 'home/news_form.html', {'form': form})
+
+def news_delete(request, id):
+    news_item = get_object_or_404(News, id=id)
+    if request.method == 'POST':
+        news_item.delete()
+        return redirect('news_list')
+    return render(request, 'home/news_confirm_delete.html', {'news': news_item})
